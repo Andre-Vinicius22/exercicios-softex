bd558d06693d7f912d33ca28447ba001022f4905
StarHealix <vinicius.bastos.monteiro@gmail.com>
2022-06-17 14:26:34 -0300

    exercicio com TAD em Python

--- tadPython.py
+++ tadPython.py
@@ -1,9 +1,21 @@
 if __name__ == '__main__':
+    # criando numeros complexos
     x = complex(4, 5)
     y = complex(8, 3)
     z = complex(2, 7)
 
+# operacoes basicas
     print(x + y)
     print(z - x)
     print(y * z)
     print(x / y)
+
+# imprimindo propiedades reais e imaginarias dos numeros
+    print(x.real)
+    print(x.imag)
+
+    print(y.real)
+    print(y.imag)
+
+    print(z.real)
+    print(z.imag)
