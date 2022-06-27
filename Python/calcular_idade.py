b4d4f966fd35a54e82564f1a81be3331807f5da3
StarHealix <vinicius.bastos.monteiro@gmail.com>
2022-06-27 18:31:12 -0300

    exercicio calcular idade

---
+++ Python/calcular_idade.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+
+    nome = input("Digite seu nome completo: ")
+    nascimento = int(input("Digite seu ano de nascimento: "))
+    idade = 2022 - nascimento
+
+print(f'Ola {nome}')
+print(f'Em 2022 voce possui ou ira completar: {idade} anos')
