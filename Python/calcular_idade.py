674362225280e9ffdff1892e86e1a618f58f0713
StarHealix <vinicius.bastos.monteiro@gmail.com>
2022-06-28 15:48:09 -0300

    exercicio calcular idade

--- Python/calcular_idade.py
+++ Python/calcular_idade.py
@@ -1,8 +1,17 @@
-if __name__ == '__main__':
-
-    nome = input("Digite seu nome completo: ")
-    nascimento = int(input("Digite seu ano de nascimento: "))
-    idade = 2022 - nascimento
-
-print(f'Ola {nome}')
-print(f'Em 2022 voce possui ou ira completar: {idade} anos')
+try:
+    loop = True
+    nome = input("Digite seu Nome completo: ")
+    while loop:
+        ano_nascimento = str(input("Digite seu ano de nascimento: "))
+        if len(ano_nascimento) == 4 and ano_nascimento.isnumeric:
+            ano_nascimento = int(ano_nascimento)
+            if (ano_nascimento > 1921) and (ano_nascimento < 2022):
+                idade = 2022 - ano_nascimento
+                print(
+                    f'Ola: {nome}, no ano de 2022 voce possui ou ira completar {idade} anos.')
+                loop = False
+        else:
+            print("insira um ano de nascimento valido")
+except Exception as e:
+    print("Ocorreu um erro")
+    print(e)
