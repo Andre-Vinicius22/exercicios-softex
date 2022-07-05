914704cf36a18d6d6642d76a207559925d1a54e4
StarHealix <vinicius.bastos.monteiro@gmail.com>
2022-07-05 13:44:48 -0300

    exercicio manipulando strings

---
+++ javascript/manipulandoStrings.js
@@ -0,0 +1,11 @@
+// transformando uma string minuscula em maiuscula
+let palavra_1 = "xicara"
+console.log(palavra_1.toUpperCase()) // XICARA
+
+// transformando uma string maiscula em minuscula
+let palavra_2 = "CAFE"
+console.log(palavra_2.toLowerCase()) // cafe
+
+// removendo uma parte de uma string
+let palavra_3 = "mergulhando em cafe"
+console.log(palavra_3.substring(0, 11)) // mergulhando
